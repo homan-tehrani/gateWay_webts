0dc9537cca5e32d7dbc7d02296bd095bc1f30c9b
picasso-mac <90257625+picasso0@users.noreply.github.com>
2024-04-02 02:49:50 +0330

    remove saveLog function

--- gateWay.py
+++ gateWay.py
@@ -240,37 +240,6 @@ class GateWay:
             return JSONResponse(content=" error in callService", status_code=400)
 
 
-def saveLog(request, message_id, request_body, response_body=''):
-    return True
-    # Get the client's IP address using a custom function (get_client_ip)
-    ip = '0'
-
-    # Try to extract the user_id from the request's scope
-    try:
-        user_id = request.scope['User'].id
-    except:
-        # If user_id extraction fails, set it to 0
-        user_id = 0
-
-    # Retrieve the log URL from environment variables
-    try:
-        # Convert request parameters and body to JSON format for logging
-        request_body_json = json.dumps(
-            {"param": request.scope['query_string'].decode(), "payload": request_body,
-             "token": request.headers.get('authorization', ''), "header": str(request.scope)})
-
-        # Prepare the payload for logging
-        payload = json.dumps(
-            {"message_id": message_id, "user_id": user_id, "request_body": request_body_json,
-             "response_body": f"{response_body}", "ip": ip, })
-
-        # Try to extract the user_id from the request's scope
-        # Set headers for the HTTP POST request to the log URL
-        headers = {'Accept': 'application/json', 'Content-Type': 'application/json'}
-        # Make an HTTP POST request to the log URL with the prepared payload
-        _response = requests.post(LOG_URL, headers=headers, data=payload)
-    except Exception as e:
-        print(" GateWayError Log connection error", str(e))
 
 
 def get_client_ip(request):
